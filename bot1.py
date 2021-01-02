00d530de6583aea2ee2569c156d00a57fd839fc7
Andrey Kuzin <51067882+Bentax@users.noreply.github.com>
2021-01-02 15:21:59 +0700

    2

--- bot1.py
+++ bot1.py
@@ -1,6 +1,15 @@
 import telebot
 
-bot = telebot.Telebot('1423318538:AAF2GgBfNEEwNtHuzrlI9sLyxgV1awmB5hI')
+bot = telebot.TeleBot('1423318538:AAF2GgBfNEEwNtHuzrlI9sLyxgV1awmB5hI')
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    sti = open('sticker.webp', 'rb')
+    bot.send_sticker(message.chat.id, sti)
+
+    bot.send_message(message.chat.id,
+                     "{0.first_name}, пиццу хочешь?\nЯ этим не занимаюсь, но ты можешь заказать где-нибудь пиццу... и мы можем поговорить...".format(message.from_user, bot.get_me()),
+                     parse_mode='html')
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
